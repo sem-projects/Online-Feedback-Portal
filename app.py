8fdcc43858ae4dc0ec2c0ced730539c6e62c2a7a
arindam-modak <arindam.modak03@gmail.com>
2018-03-14 22:38:47 +0530

    template rendered in app.py and static added

--- app.py
+++ app.py
@@ -1,9 +1,27 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask,render_template,redirect, url_for
+app = Flask(__name__, static_url_path='/static')
 
 @app.route('/')
-def hello_world():
-   return ("hello world")
+def index():
+   return redirect(url_for('login'))
+
+@app.route('/login')
+def login():
+	return render_template("index.html")
+
+
+@app.route('/register')
+def register():
+	return ("login")
+
+
+@app.route('/dashboard/<id>')
+def dashboard(id):
+	return ("login")
+
+
 
 if __name__ == '__main__':
-   app.run()
+	app.debug = True
+	app.run()
+	app.run(debug = True)
