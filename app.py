ef94be46ae9eebcebc195c7f0bf110927b73802a
xxator <parth.agarwal131@gmail.com>
2018-03-14 23:56:38 +0000

    update

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
-from flask import Flask,render_template,redirect, url_for
+from flask import Flask,render_template,redirect, url_for,request
 app = Flask(__name__, static_url_path='/static')
 
 @app.route('/')
 def index():
    return redirect(url_for('login'))
 
-@app.route('/login')
+@app.route('/login',methods = ['GET','POST'])
 def login():
+	if request.method == 'POST':
+		username = request.form['username']
+		password = request.form['password']
 	return render_template("login.html")
 
 
