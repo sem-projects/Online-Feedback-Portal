83e17b79061e847e86f9d6749a3007f05df9e4ed
arindam-modak <arindam.modak03@gmail.com>
2018-03-15 01:19:43 +0530

    register linked

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
-<<<<<<< HEAD
 from flask import Flask,render_template,redirect, url_for, request
 import sqlite3
-=======
 from flask import Flask,render_template,redirect, url_for,request
->>>>>>> 9905a7b5a4ff0136271915c6d4fa784906f27c05
 app = Flask(__name__, static_url_path='/static')
 
 
@@ -20,24 +17,16 @@ def index():
 
 @app.route('/login',methods = ['GET','POST'])
 def login():
-<<<<<<< HEAD
-
 	if request.method == 'POST':
 		username=request.form['username']
 		password=request.form['pass']
-		
-=======
-	if request.method == 'POST':
-		username = request.form['username']
-		password = request.form['password']
->>>>>>> 9905a7b5a4ff0136271915c6d4fa784906f27c05
 	return render_template("login.html")
       
 
 
 @app.route('/register')
 def register():
-	return ("login")
+	return render_template("student_register.html")
 
 
 @app.route('/dashboard/<id>')
