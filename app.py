6972dace3ea702550b9a9efaab52a6e8d5942fe1
Arindam Das Modak <iit2016036@iiita.ac.in>
2018-04-21 15:11:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -193,8 +193,8 @@ def new():
 
 app.config['MAIL_SERVER']='smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
-app.config['MAIL_USERNAME'] = 'iit2016047@iiita.ac.in'
-app.config['MAIL_PASSWORD'] = 'cricketstar'
+app.config['MAIL_USERNAME'] = ''
+app.config['MAIL_PASSWORD'] = ''
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
 
